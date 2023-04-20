b7746b25e8ee98a5fb3de04b2e1603a7e059d380
Stanislav <boykostas@gmail.com>
2023-04-20 15:23:54 +0300

    Добавил идущее время на сайт, доделать в буквенном выражении

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict'
+// Функция идущего времени на главной странице
+function startTime() {
+    var tm = new Date();
+    var day = tm.getDate();
+    var month = tm.getMonth();
+    var year = tm.getFullYear();
+    var week = tm.getDay();
+    var h = tm.getHours();
+    var m = tm.getMinutes();
+    var s = tm.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timer').innerHTML = day + "." + month + "." + year + " " + week + " " + h + ":" + m + ":" + s;
+    t = setTimeout('startTime()', 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+// https://itchief.ru/javascript/date
