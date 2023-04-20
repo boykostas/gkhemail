0d4153d48d822c725f7e8ca3a2ba21f63f8f0cf8
Stanislav <boykostas@gmail.com>
2023-04-20 15:38:15 +0300

    Добавил идущее время на сайт, доделать в буквенном выражении

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 function startTime() {
     var tm = new Date();
     var day = tm.getDate();
-    var month = tm.getMonth();
+    var month = tm.getMonth() + 1;
+    if (month < 10) month = '0' + month;
     var year = tm.getFullYear();
     var week = tm.getDay();
     var h = tm.getHours();
@@ -22,3 +23,25 @@ function checkTime(i) {
 }
 
 // https://itchief.ru/javascript/date
+
+// var month = new Array(12);
+// month[0] = "Января";
+// month[1] = "Февраля";
+// month[2] = "Марта";
+// month[3] = "Апреля";
+// month[4] = "Мая";
+// month[5] = "Июня";
+// month[6] = "Июля";
+// month[7] = "Августа";
+// month[8] = "Сентября";
+// month[9] = "Октября";
+// month[11] = "Декабря";
+
+// var weekday = new Array(7);
+// weekday[0] = "Воскресенье";
+// weekday[1] = "Понедельник";
+// weekday[2] = "Вторник";
+// weekday[3] = "Среда";
+// weekday[4] = "Четверг";
+// weekday[5] = "Пятница";
+// weekday[6] = "Суббота";
