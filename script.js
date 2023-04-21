c5d8a81ddc189a69951b173c5c01016177c46598
Stanislav <boykostas@gmail.com>
2023-04-21 09:18:10 +0300

    Добавил идущее время и дату на сайт

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 'use strict'
 // Функция идущего времени на главной странице
 function startTime() {
-    var tm = new Date();
-    var day = tm.getDate();
-    var month = tm.getMonth() + 1;
-    if (month < 10) month = '0' + month;
-    var year = tm.getFullYear();
-    var week = tm.getDay();
-    var h = tm.getHours();
-    var m = tm.getMinutes();
-    var s = tm.getSeconds();
+    var date = new Date();
+    var day = date.getDate();
+    var monthOfYear = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+    var month = monthOfYear[date.getMonth()];
+    var year = date.getFullYear();
+    var weekdays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    var week = weekdays[date.getDay()];
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('timer').innerHTML = day + "." + month + "." + year + ", " + week + ", " + h + ":" + m + ":" + s;
+    document.getElementById('timer').innerHTML = day + " " + month + " " + year + " года, " + week + ", " + h + ":" + m + ":" + s;
     t = setTimeout('startTime()', 500);
 }
 function checkTime(i) {
@@ -21,27 +22,3 @@ function checkTime(i) {
     }
     return i;
 }
-
-// https://itchief.ru/javascript/date
-
-// var month = new Array(12);
-// month[0] = "Января";
-// month[1] = "Февраля";
-// month[2] = "Марта";
-// month[3] = "Апреля";
-// month[4] = "Мая";
-// month[5] = "Июня";
-// month[6] = "Июля";
-// month[7] = "Августа";
-// month[8] = "Сентября";
-// month[9] = "Октября";
-// month[11] = "Декабря";
-
-// var weekday = new Array(7);
-// weekday[0] = "Воскресенье";
-// weekday[1] = "Понедельник";
-// weekday[2] = "Вторник";
-// weekday[3] = "Среда";
-// weekday[4] = "Четверг";
-// weekday[5] = "Пятница";
-// weekday[6] = "Суббота";
